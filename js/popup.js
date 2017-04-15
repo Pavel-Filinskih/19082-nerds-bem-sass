bf14c84057e8b56ff7cad8f2a95050eed9f3a1f5
Pavel-Filinskih <pavfilin@gmail.com>
2017-04-15 16:47:43 +0600

    верстка по бэм v3

--- js/popup.js
+++ js/popup.js
@@ -2,18 +2,18 @@
 
 var mailButton = document.querySelector('.map-mail__send');
 var popup = document.querySelector('.modal-window');
-var close = document.querySelector('.modal-close');
+var close = document.querySelector('.modal-form-close');
 var form = popup.querySelector('.modal-form');
-var username = popup.querySelector('[name=username]');
-var mailto = popup.querySelector('[name=mail]');
+var username = popup.querySelector('.modal-form-username-field__input');
+var mailto = popup.querySelector('.modal-form-mail-field__input');
 
 mailButton.addEventListener('click', function(event) {
 	event.preventDefault();
-	popup.classList.add('modal-show');
+	popup.classList.add('modal-form-show');
 	username.focus();
 });
 
 close.addEventListener('click', function(event) {
 	event.preventDefault();
-	popup.classList.remove('modal-show');
+	popup.classList.remove('modal-form-show');
 });
