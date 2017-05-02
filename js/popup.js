bc4115450264dc12c5012ab5242c53586fe30fe4
Pavel-Filinskih <pavfilin@gmail.com>
2017-05-02 15:12:27 +0600

    верстка по бэм v4

--- js/popup.js
+++ js/popup.js
@@ -7,13 +7,21 @@ var form = popup.querySelector('.modal-form');
 var username = popup.querySelector('.modal-form-username-field__input');
 var mailto = popup.querySelector('.modal-form-mail-field__input');
 
+var removeElement = function (elem) {
+  elem.classList.remove('modal-form-show');
+}
+
+var addElement = function (elem) {
+  elem.classList.add('modal-form-show');
+}
+
 mailButton.addEventListener('click', function(event) {
 	event.preventDefault();
-	popup.classList.add('modal-form-show');
 	username.focus();
+  addElement(popup);
 });
 
 close.addEventListener('click', function(event) {
 	event.preventDefault();
-	popup.classList.remove('modal-form-show');
+	removeElement(popup);
 });
