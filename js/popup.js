08098fee16865badae007fb3ae2f1658b737b4b3
Pavel-Filinskih <pavfilin@gmail.com>
2017-05-02 15:41:28 +0600

    правка скрипта popup v1

--- js/popup.js
+++ js/popup.js
@@ -1,27 +1,28 @@
 'use strict';
 
 var mailButton = document.querySelector('.map-mail__send');
-var popup = document.querySelector('.modal-window');
-var close = document.querySelector('.modal-form-close');
-var form = popup.querySelector('.modal-form');
-var username = popup.querySelector('.modal-form-username-field__input');
-var mailto = popup.querySelector('.modal-form-mail-field__input');
 
-var removeElement = function (elem) {
+var modalWindow = document.querySelector('.modal-window');
+var modalWindowClose = document.querySelector('.modal-form-close');
+
+var modalForm = modalWindow.querySelector('.modal-form');
+var modalUserName = modalWindow.querySelector('.modal-form-username-field__input');
+var modalUserEmail = modalWindow.querySelector('.modal-form-mail-field__input');
+
+var hideElement = function (elem) {
   elem.classList.remove('modal-form-show');
 }
 
-var addElement = function (elem) {
+var showElement = function (elem) {
   elem.classList.add('modal-form-show');
 }
 
 mailButton.addEventListener('click', function(event) {
 	event.preventDefault();
-	username.focus();
-  addElement(popup);
+  showElement(modalWindow);
 });
 
-close.addEventListener('click', function(event) {
+modalWindowClose.addEventListener('click', function(event) {
 	event.preventDefault();
-	removeElement(popup);
+	hideElement(modalWindow);
 });
